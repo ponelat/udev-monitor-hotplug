0f76c486d045957999f740ca12ba904c4ed2bf16
Josh Ponelat <jponelat@gmail.com>
2017-01-18 07:40:44 +0200

    add my local rules for monitors

--- usr/local/bin/monitor-hotplug.sh
+++ usr/local/bin/monitor-hotplug.sh
@@ -14,7 +14,7 @@ export DISPLAY=":$displaynum.0"
 export XAUTHORITY=$(ps -C Xorg -f --no-header | sed -n 's/.*-auth //; s/ -[^ ].*//; p')
 
 
-#this while loop declare the $HDMI1 $VGA1 $LVDS1 and others if they are plugged in
+#this while loop declare the $HDMI1 $VGA1 $eDP1 and others if they are plugged in
 while read l
 do
   dir=$(dirname $l);
@@ -38,26 +38,26 @@ do
 done <<< "$DEVICES"
 
 
-if [ ! -z "$HDMI1" -a ! -z "$VGA1" ]
+if [ ! -z "$HDMI1" -a ! -z "$DP1" ]
 then
-  echo "HDMI1 and VGA1 are plugged in"
-  xrandr --output LVDS1 --off
-  xrandr --output VGA1 --mode 1920x1080 --noprimary
-  xrandr --output HDMI1 --mode 1920x1080 --right-of VGA1 --primary
-elif [ ! -z "$HDMI1" -a -z "$VGA1" ]; then
-  echo "HDMI1 is plugged in, but not VGA1"
-  xrandr --output LVDS1 --off
-  xrandr --output VGA1 --off
-  xrandr --output HDMI1 --mode 1920x1080 --primary
-elif [ -z "$HDMI1" -a ! -z "$VGA1" ]; then
-  echo "VGA1 is plugged in, but not HDMI1"
-  xrandr --output LVDS1 --off
-  xrandr --output HDMI1 --off
-  xrandr --output VGA1 --mode 1920x1080 --primary
+  echo "HDMI1 and DP1 are plugged in"
+  xrandr --output eDP1 --auto \
+         --output HDMI1 --mode 1920x1080 --right-of eDP1 --primary \
+         --output DP1 --mode 1920x1080 --right-of HDMI1 --noprimary
+elif [ ! -z "$HDMI1" -a -z "$DP1" ]; then
+  echo "HDMI1 is plugged in, but not DP1"
+  xrandr --output eDP1 --auto \
+         --output HDMI1 --mode 1920x1080 --right-of eDP1 --primary \
+         --output DP1 --off
+elif [ -z "$HDMI1" -a ! -z "$DP1" ]; then
+  echo "DP1 is plugged in, but not HDMI1"
+  xrandr --output eDP1 --auto \
+         --output HDMI1 --off \
+         --output DP1 --mode 1920x1080 --right-of eDP1 --primary 
 else
   echo "No external monitors are plugged in"
-  xrandr --output LVDS1 --off
-  xrandr --output HDMI1 --off
-  xrandr --output LVDS1 --mode 1366x768 --primary
+  xrandr --output eDP1 --auto --primary \
+         --output HDMI1 --off \
+         --output DP1 --off
 fi
 
